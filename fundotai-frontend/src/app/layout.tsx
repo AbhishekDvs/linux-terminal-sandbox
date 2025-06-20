01f0097143ce897f4bc690110fc1640d170a9c88
abhishekdvs <abhishek280194@gmail.com>
2025-06-20 22:25:26 +0530

    meta tag updated

--- fundotai-frontend/src/app/layout.tsx
+++ fundotai-frontend/src/app/layout.tsx
@@ -13,22 +13,22 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Linux Terminal Sandbox",
-  description: "Try out linux commands in your web browser!",
+  title: 'Linux Terminal Sandbox',
+  description: 'Try out linux commands in your web browser!',
   icons: {
     icon: '/favicon.ico',
   },
   openGraph: {
     title: 'Linux Terminal Sandbox',
-    description: 'Try out Linux commands directly in your web browser!',
+    description: 'Try out linux commands in your web browser!',
     url: 'https://terminalsandbox.pages.dev',
     siteName: 'Linux Terminal Sandbox',
     images: [
       {
-        url: '/og-image.png', // ✅ Place this image in your /public folder
+        url: 'https://terminalsandbox.pages.dev/og-image.png',
         width: 1200,
         height: 630,
-        alt: 'Linux Terminal UI Preview',
+        alt: 'Linux Terminal Preview',
       },
     ],
     type: 'website',
@@ -36,10 +36,11 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'Linux Terminal Sandbox',
-    description: 'Try out Linux commands directly in your browser!',
-    images: ['/og-image.png'], // Same as above
+    description: 'Try out linux commands in your web browser!',
+    images: ['https://terminalsandbox.pages.dev/og-image.png'],
   },
 };
+
 
 
 export default function RootLayout({
